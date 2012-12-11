b8782e705865e33505cbec11cbcc03c37fef6ee0
wanglifeng <tytc011@163.com>
2012-12-11 15:46:53 +0800

    add register controller and sha1 algorithm

---
+++ DomainCoreTest/SHA1AlgorithmTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainCore;
+
+namespace DomainCoreTest
+{
+    [TestClass]
+    public class SHA1AlgorithmTest
+    {
+        [TestMethod]
+        public void EnctyptTest()
+        {
+            String str = "HelloWorld";
+            Assert.AreEqual(string.Empty, SHA1Algorithm.Enctypt(str));
+        }
+    }
+}
---
+++ WeiChatOpenPlatform/Controllers/RegisterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using DomainCore;
+
+namespace WeiChatOpenPlatform.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private const string TOKEN = "CareerBuilderChina";
+        private const String ERROR_CONTENT = "ERROR";
+        public ActionResult Index(String signature, string timestamp, string nonce, string echostr)
+        {
+            if (String.IsNullOrEmpty(signature) || String.IsNullOrEmpty(timestamp)
+                || String.IsNullOrEmpty(nonce) || String.IsNullOrEmpty(echostr))
+            {
+                String[] qsValues = { TOKEN, timestamp, nonce };
+                Array.Sort<String>(qsValues);
+                String plainString = String.Join("", qsValues);
+                String encryptedString = SHA1Algorithm.Enctypt(plainString);
+                if (encryptedString == signature)
+                {
+                    return new ContentResult() { Content = echostr };
+                }
+            }
+            return new ContentResult() { Content = ERROR_CONTENT };
+        }
+
+    }
+}
