ca1bd071792d287cc1d3986140a31bc73de1ec26
wang lifeng <tytc011@163.com>
2012-12-13 18:16:59 +0800

    add some filters

---
+++ DomainCore/Filters/LocationFilters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DomainCore.DB;
+
+namespace DomainCore.Filters
+{
+    class LocationFilters : IFilter
+    {
+        public bool Filters(string userId, string content, out  string result)
+        {
+            User user = DataBase.Instance.Users.SingleOrDefault(t => t.ID == userId);
+            if (user != null)
+            {
+                if (content == "上海" || content == "shanghai")
+                {
+                    Transaction transaction = DataBase.Instance.Transaction.SingleOrDefault(t => t.UserId == userId);
+                    if (transaction != null && transaction.Status == Status.KeyWordSet)
+                    {
+                        transaction.Location = content;
+                        transaction.Status = Status.Done;
+                        result = "这里是职位结果";
+                        return true;
+                    }
+                }
+
+            }
+            result = string.Empty;
+            return false;
+        }
+    }
+}
--- DomainCore/Reboter.cs
+++ DomainCore/Reboter.cs
@@ -3,21 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainCore.Filters;
 
 namespace DomainCore
 {
     public class Reboter
     {
+
+        public static Reboter _Reboter { get; set; }
+
+        public  List<IFilter> _Filters { get; set; }
+
+        private Reboter()
+        {
+            _Filters = new List<IFilter>();
+            _Filters.Add(new ConstContentFilter());
+            _Filters.Add(new BeginTransaction());
+            _Filters.Add(new KeyWordFilter());
+            _Filters.Add(new LocationFilters());
+            _Filters.Add(new UnKnowFilter());
+        }
+
+        public static Reboter Instance
+        {
+            get
+            {
+                if (_Reboter == null)
+                    _Reboter = new Reboter();
+
+                return _Reboter;
+            }
+        }
+
         public static String TryToUnderStand(String from, string content)
         {
-            if (content == "你是谁")
+            String resultContent=string.Empty;
+
+            foreach (var t in Reboter.Instance._Filters)
             {
-                return "我是小C机器人";
+                if (t.Filters(from, content, out resultContent))
+                {
+                    break;
+                }
             }
-            else
-            {
-                return "我正在理解您的内容";
-            }
+
+            return resultContent;
+            
         }
     }
 }
